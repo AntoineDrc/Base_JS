76eaa580f2189baf260f88be750e2821a3ca21b2
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-03-14 14:32:07 +0100

    fin exercice 2

--- exo2/main.js
+++ exo2/main.js
@@ -1,15 +1,23 @@
 // Afficher le code couleur dans le carre cliqué
 
-    // Ajouter un écouteur d'évènement pour réagir au clic
-    carre.addEventListener('click', function()
+    // Cibler tout les éléments .carré
+    let carres = document.querySelectorAll('.carre');
+    let carreBas = document.querySelector('.carre_bas');
+
+    // Ajouter un écouteur d'évènement sur chaque élèment portant la class carre
+    carres.forEach(function(carre)
     {
-
-        // Cibler tout les éléments .carré
-        const styleCarre=[];
-
-        // Obtenir le style CSS d'une class carre
-        let style = getComputedStyle(carre);
-            let bcColor = style.backgroundColor;
-    });
-
+        carre.addEventListener('click', function()
+        {
     
+            // Obtenir le style CSS d'une class carre
+            let style = getComputedStyle(carre);
+            let bgColor = style.backgroundColor;
+
+            // Changer la couleur du carre bas
+            carreBas.style.backgroundColor = bgColor;
+
+            // Afficher le code couleur
+            carreBas.innerText = bgColor;
+        });
+    });
