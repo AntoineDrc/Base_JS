c96da8a5f3b80104ced9581a06b67ca216feddda
Antoine Delubriac <Antoine.DELUBRIAC@stagiaire-elan.fr>
2024-03-15 09:31:44 +0100

    affichage du nom du réseau

--- exo4/main.js
+++ exo4/main.js
@@ -25,5 +25,46 @@ cards.forEach(function(card)
                 {
                     fond.style.backgroundImage = '';
                 }
-            })
-})
+
+                // Arrondir les bords
+                if(!bool)
+                {
+                    card.style.borderRadius = '20px';
+                } else 
+                {
+                    card.style.borderRadius = '';
+                }
+
+                // Appliquer une ombre
+                if(!bool)
+                {
+                    card.style.boxShadow = 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px';
+                } else 
+                {
+                    card.style.boxShadow = '';
+                }
+
+                // Le nom du réseau social apparait
+
+                // On récupère le nom du réseau dans l'id
+                let nomReseau = card.getAttribute('id');
+
+                // Création d'un enfant "span" pour afficher le nom du réseau
+                let nomElement = document.createElement('span');
+
+                // On ajoute la span créé comment enfant le la class "card"
+                card.appendChild(nomElement);
+
+
+                if (!bool)
+                {
+                    nomElement.textContent = nomReseau;
+                } else
+                {
+                    if (nomElement)
+                    {
+                        card.removeChild(nomElement);
+                    }
+                }
+            });
+});
